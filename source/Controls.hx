f9d158fc00d1acdc8f9a8885c1440641c421b647
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2023-01-17 20:35:27 -0300

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -760,13 +760,13 @@ class Controls extends FlxActionSet
 
 	public function unbindKeys(control:Control, keys:Array<FlxKey>)
 	{
-		#if (haxe >= "4.0.0")                                                                                                                                                           (action, _) -> removeKeys(action, keys));
+		#if (haxe >= "4.0.0")
+		inline forEachBound(control, (action, _) -> removeKeys(action, keys));
 		#else
 		forEachBound(control, function(action, _) removeKeys(action, keys));
 		#end
 	}	
-	#end
-
+	#end	
 	inline static function addKeys(action:FlxActionDigital, keys:Array<FlxKey>, state:FlxInputState)
 	{
 		for (key in keys)
