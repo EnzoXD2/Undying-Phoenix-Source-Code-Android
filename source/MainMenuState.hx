20956c87f790562b383936af9c26a30698e4e8a7
Enzo Mods <96305131+EnzoXD2@users.noreply.github.com>
2023-01-17 22:55:04 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -154,7 +154,7 @@ class MainMenuState extends MusicBeatState
 		#end
 			
 		#if android
-                addVirtualPad(UP_DOWN, A_B_X_Y);
+                addVirtualPad(UP_DOWN, A_B);
                 #end
 
 		super.create();
