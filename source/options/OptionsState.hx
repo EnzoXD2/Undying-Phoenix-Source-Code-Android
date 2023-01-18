3cfe40d9cc75fef5503deeed33f38f46ae45df3c
Enzo Mods <96305131+EnzoXD2@users.noreply.github.com>
2023-01-17 22:33:16 -0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -102,7 +102,7 @@ class OptionsState extends MusicBeatState
 		ClientPrefs.saveSettings();
 		
 		#if android
-                addVirtualPad(LEFT_FULL, A_B_C);
+                addVirtualPad(UP_DOWN, A_B_C);
                 #end
 
 		super.create();
